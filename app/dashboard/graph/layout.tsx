687dc3a81b4d55f4155e90e5aa82619841de581c
reza nahi <m.rezanahi.d@gmail.com>
2026-05-24 17:50:20 +0330

    fix graph one note view header

--- app/dashboard/graph/layout.tsx
+++ app/dashboard/graph/layout.tsx
@@ -16,11 +16,11 @@ export default function ({children} : any) {
     return (
         <>
             <div className="flex flex-col justify-start gap-3 h-full overflow-y-auto relative">
-                <div className={`h-24 !z-50 bg-transparent absolute top-0 left-0 right-0 flex justify-start items-center shrink-0 flex gap-1 justify-end items-center px-6`}>
+                <div className={`h-auto !z-50 absolute top-7 left-0 right-0 flex justify-start items-center shrink-0 flex gap-1 justify-end items-center px-6`}>
                     <h3 className={'font-bold text-lg'}>Graph View</h3>
                     {hasId && NoteIsSuccess && <p> - {NoteData?.data?.note?.title}</p>}
                     {
-                        hasId && NoteIsSuccess &&
+                        (hasId && NoteIsSuccess) &&
                         <button
                             className="flex justify-between items-center rounded-md cursor-pointer ml-auto w-auto text-left px-3 py-2 hover:bg-[#3a3a3a] text-gray-200"
                             onClick={() => {router.push(`/dashboard/graph`)}}
