3a274bffc27f15780ba232bd16a23f296444912b
reza nahi <m.rezanahi.d@gmail.com>
2026-05-24 16:18:20 +0330

    fix horizontal scroll in note page

--- components/MarkdownEditor.tsx
+++ components/MarkdownEditor.tsx
@@ -2,44 +2,31 @@
 
 import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
-import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
-import { oneDark } from "@codemirror/theme-one-dark";
-import { languages } from "@codemirror/language-data";
+import { markdown } from "@codemirror/lang-markdown";
+import { EditorView } from "@codemirror/view";
 
-interface MarkdownEditor {
-    value: string,
-    onChange: (value: string) => void
-    className?: string
+interface MarkdownEditorProps {
+    value: string;
+    onChange: (value: string) => void;
+    className?: string;
 }
 
-export default function MarkdownEditor({ value, onChange, className='' } : MarkdownEditor) {
+export default function MarkdownEditor({ value, onChange, className = "" }: MarkdownEditorProps) {
     return (
-        <div className="h-full">
-            {/*<CodeMirror*/}
-            {/*    value={value}*/}
-            {/*    height="100%"*/}
-            {/*    theme={oneDark}*/}
-            {/*    basicSetup={{*/}
-            {/*        lineNumbers: true,*/}
-            {/*        highlightActiveLine: true,*/}
-            {/*        foldGutter: true,*/}
-            {/*    }}*/}
-            {/*    extensions={[*/}
-            {/*        markdown({ base: markdownLanguage, codeLanguages: languages }),*/}
-            {/*    ]}*/}
-            {/*    onChange={(val) => onChange(val)}*/}
-            {/*    className={`border border-gray-700 rounded-md ${className}`}*/}
-            {/*/>*/}
+        <div className="h-full w-full overflow-hidden">
             <CodeMirror
                 value={value}
                 onChange={onChange}
-                extensions={[markdown()]}
+                extensions={[
+                    markdown(),
+                    EditorView.lineWrapping // مهم‌ترین بخش برای شکستن خطوط
+                ]}
                 basicSetup={{
                     lineNumbers: false,
                     foldGutter: false,
                     highlightActiveLine: false
                 }}
-                className="obsidian-editor"
+                className={`obsidian-editor whitespace-pre-wrap break-words overflow-x-hidden ${className}`}
             />
         </div>
     );
