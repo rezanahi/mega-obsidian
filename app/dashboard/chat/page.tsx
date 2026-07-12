02dd7e6bbcf8acada1b5ec3bd1bfaf9a3a9fd8dd
reza nahi <m.rezanahi.d@gmail.com>
2026-07-12 14:14:35 +0330

    feat : add voice chat with ai in chat page

--- app/dashboard/chat/page.tsx
+++ app/dashboard/chat/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 import { Input, Spin } from 'antd';
-import {useState} from "react";
-import {ChatAvalAiApi} from "@/apis";
+import {useEffect, useRef, useState} from "react";
+import {ChatAvalAiApi, voiceToTextAvalAi} from "@/apis";
 import Link from "next/link";
-import {ArrowRightOutlined} from "@ant-design/icons";
+import {ArrowRightOutlined, AudioOutlined, LoadingOutlined} from "@ant-design/icons";
+import {formatTime} from "@/utils/methods";
+import {RecordCircleIcon} from "@/components/icons";
+import type { InputRef } from "antd";
 
 
 const { TextArea } = Input;
@@ -12,14 +15,95 @@ const { TextArea } = Input;
 export default function ChatPage () {
 
     const [prompt, setPrompt] = useState<string>('')
-
+    const [recording, setRecording] = useState<boolean>(false)
+    const [recordTime, setRecordTime] = useState(0)
+    const timerRef = useRef<NodeJS.Timeout | null>(null)
+    const mediaRecorderRef = useRef<MediaRecorder | null>(null)
+    const chunksRef = useRef<Blob[]>([])
+    const textAreaRef = useRef<InputRef>(null);
     // Apis
     const { mutate: chatApi, isSuccess: chatApiSuccess, isPending: chatApiIsLoading, data: chatApiData } = ChatAvalAiApi()
+    const { mutate : voiceToTextAvalAiMutate, data: voiceToTextAvalAiData, isSuccess: voiceToTextAvalAiIsSuccess, isPending: voiceToTextAvalAiIsPending, isError: voiceToTextAvalAiIsError } = voiceToTextAvalAi()
 
     function SendPromptHandler () {
         console.log('Enter Handled')
         chatApi({question: prompt})
         setPrompt('')
+    }
+
+    // Sync voice to text response with prompt state
+    useEffect(() => {
+        if (voiceToTextAvalAiIsSuccess && !!voiceToTextAvalAiData) {
+            setPrompt(voiceToTextAvalAiData?.data?.text)
+            requestAnimationFrame(() => {
+                textAreaRef.current?.focus();
+            });
+
+        }
+    }, [voiceToTextAvalAiIsSuccess, voiceToTextAvalAiData])
+
+    async function sendAudioToAPI(blob: Blob) {
+        const file = new File([blob], "voice.webm", { type: "audio/webm" })
+
+        const formData = new FormData()
+        formData.append("audio", file)
+
+        // const res = await fetch("/api/audio-to-text", {
+        //     method: "POST",
+        //     body: formData,
+        // })
+        console.log("YOHAHA = ", file)
+        voiceToTextAvalAiMutate(file)
+
+        // const data = voiceToTextData
+        // console.log("AI text:", data.text)
+
+        // return data.text
+    }
+
+    async function startRecording() {
+        // اگر تایمر قبلی هنوز فعال است، پاکش کن
+        if (timerRef.current) {
+            clearInterval(timerRef.current);
+        }
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+        const mediaRecorder = new MediaRecorder(stream)
+        mediaRecorderRef.current = mediaRecorder
+        chunksRef.current = []
+        mediaRecorder.ondataavailable = (event) => {
+            chunksRef.current.push(event.data)
+        }
+        mediaRecorder.onstop = async () => {
+            // ساخت Blob واقعی بعد از اینکه Recorder کامل داده‌ها را جمع کرد
+            const blob = new Blob(chunksRef.current, { type: "audio/webm" })
+            console.log("Recorded Blob:", blob, "size:", blob.size)
+            // اگر size=0 → یعنی میکروفن mute، یا start/stop خیلی سریع
+            // const text = await sendAudioToAPI(blob)
+            sendAudioToAPI(blob)
+            // if (text) {
+            //     setNoteContent(prev => {
+            //         const newContent = (prev || "") + "\n" + text
+            //         return newContent
+            //     })
+            // }
+        }
+        mediaRecorder.start()
+        setRecording(true)
+        setRecordTime(0)
+        timerRef.current = setInterval(() => {
+            setRecordTime(prev => prev + 1)
+        }, 1000)
+    }
+
+    async function stopRecording() {
+        if (mediaRecorderRef.current) {
+            mediaRecorderRef.current.stop()
+        }
+        setRecording(false)
+        if (timerRef.current) {
+            console.log("=== I Deleted It ===")
+            clearInterval(timerRef.current)
+        }
     }
 
     return (
@@ -28,15 +112,43 @@ export default function ChatPage () {
                 <h1 className="text-3xl md:text-5xl font-bold tracking-tighter">
                     MegaBot
                 </h1>
-                <TextArea
-                    disabled={chatApiIsLoading}
-                    onPressEnter={() => SendPromptHandler()}
-                    placeholder={'Ask...'}
-                    className={'w-full text-wrap h-auto overflow-hidden'}
-                    autoFocus
-                    value={prompt}
-                    onChange={(e) => setPrompt(e.target.value)}>
-                </TextArea>
+                <div className={'relative w-full'}>
+                    <div className={'absolute z-50! bg-gray-600/20 flex justify-center items-center aspect-square rounded-md right-1 top-1'}>
+                        {
+                            voiceToTextAvalAiIsPending ?
+                                <Spin indicator={<LoadingOutlined spin />} size={"small"}></Spin> :
+                                recording ?
+                                    <div className={'flex justify-start items-center gap-2 '}>
+                                        <button
+                                            className="flex justify-between items-center rounded-md cursor-pointer w-auto text-left px-3 py-2 hover:bg-[#3a3a3a] text-gray-200"
+                                            onClick={() => {
+                                                stopRecording()
+                                            }}
+                                        >
+                                            <RecordCircleIcon width={16} height={16} fill={"red"}></RecordCircleIcon>
+                                        </button>
+                                    </div>
+                                    : <button
+                                        className="flex justify-between items-center rounded-md cursor-pointer w-auto text-left px-3 py-2 hover:bg-[#3a3a3a] text-gray-200"
+                                        onClick={() => {
+                                            startRecording()
+                                        }}
+                                    >
+                                        <AudioOutlined></AudioOutlined>
+                                    </button>
+                        }
+                    </div>
+                    <TextArea
+                        ref={textAreaRef}
+                        disabled={chatApiIsLoading}
+                        onPressEnter={() => SendPromptHandler()}
+                        placeholder={'Ask...'}
+                        className={'w-full text-wrap h-auto overflow-hidden pr-10!'}
+                        autoFocus
+                        value={prompt}
+                        onChange={(e) => setPrompt(e.target.value)}>
+                    </TextArea>
+                </div>
                 {
                     chatApiIsLoading ?
                         <Spin></Spin> :
